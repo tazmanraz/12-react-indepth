3015c4abcfd2b45da3f7a54b59927376f0e8f1be
Imtiaz <imtiazmiah@gmail.com>
2021-10-13 20:31:37 -0400

    Understanding useCallback dependancy, closure functions

--- src/App.js
+++ src/App.js
@@ -6,16 +6,24 @@ import './App.css';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   // deals with prop values being comparable and allowing react.memo work properly in children components
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph(prevShowParagraph => !prevShowParagraph)
-  },[]);
+    if (allowToggle) {
+      setShowParagraph(prevShowParagraph => !prevShowParagraph)
+    }
+  },[allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true)
+  }
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
